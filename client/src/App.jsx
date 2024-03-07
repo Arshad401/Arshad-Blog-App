9616c960e11d960f9c9e9d87f6027cf7e2f39283
Arshad401 <arshadxx91@gmail.com>
2024-03-07 09:50:22 +0400

    ceate Header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,14 @@ import Projects from './pages/Projects';
 import Dashboard from './pages/Dashboard';
 import Signin from "./pages/SignIn";
 import Signup from "./pages/Signup";
+import Header from "./components/Header";
 
 
 const App = () => {
   return (
 
  <BrowserRouter>
+ <Header />
  <Routes>
 
   <Route path='/' element={<Home />} />
